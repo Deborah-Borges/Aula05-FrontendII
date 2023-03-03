2a197d2ca824d11ae655b958e1a1095c7968c3bc
Deborah-Borges <pretinha.deb@gmail.com>
2023-03-03 16:05:46 -0300

    subindo repo

---
+++ Aula04/dom.js
@@ -0,0 +1,28 @@
+// Obtendo o Body
+document.querySelector('body')
+
+    // Obtendo h1
+    document.querySelector('h1')
+        // conteúdo h1
+        document.querySelector('h1').textContent
+
+    // obtendo a div class="conteudo"
+    document.querySelector('.conteudo')
+
+        //Obtendo a ul
+        document.querySelector('ul')
+
+            // Obtendo a li
+            document.querySelectorAll('.conteudo li')
+
+    // Obtendo a section
+    document.querySelector('section')
+
+        // Obtendo o arquivo com id card1
+        document.querySelectorAll('article')
+
+        // Obtendo o h2
+        document.querySelectorAll('article h2')
+
+        // Obtendo o p das tags article
+        document.querySelectorAll('article p')
